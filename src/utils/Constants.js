5ded433e452dedcba558551203307a6e2fa46df4
KChavda <k.chavda6859@gmail.com>
2024-05-29 23:14:22 +0530

    Modified BASE_URL for API Calls.

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://quizzie-ef8r.onrender.com/api/v1";
 const TOKEN = localStorage.getItem("token");
 const USERID = localStorage.getItem("userId");
 
